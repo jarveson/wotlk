e926cbb57c6b10fa044836852988de865c3bb28f
jarves <jarveson@gmail.com>
2023-09-27 23:18:59 -0500

    pass char by ptr
    Signed-off-by: jarves <jarveson@gmail.com>

--- sim/priest/priest.go
+++ sim/priest/priest.go
@@ -177,9 +177,9 @@ func (priest *Priest) AddShadowWeavingStack(sim *core.Simulation) {
 func (priest *Priest) Reset(_ *core.Simulation) {
 }
 
-func New(char core.Character, selfBuffs SelfBuffs, talents string) *Priest {
+func New(char *core.Character, selfBuffs SelfBuffs, talents string) *Priest {
 	priest := &Priest{
-		Character: char,
+		Character: *char,
 		SelfBuffs: selfBuffs,
 		Talents:   &proto.PriestTalents{},
 	}
